93d9d11ebf43fc7dbb234a33de4281254a58694d
Clinton Johnson <iamcmj@icloud.com>
2019-12-26 12:32:34 -0500

    updating custom route

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const port = 3000;
 app.get('/', (req, res) => {
 	res.send('Hello for express clinton');
 });
+app.get('/demo', (req, res) => {
+	res.set('X-full-stock', '4life');
+	res.status(418);
+	res.send('I prefer coffee');
+});
 
 app.listen(port, () => console.log('App has started on port', port));
